65dae5aad0cf269d1a1e91e75005525734b096be
Simon-Ghobreil <simon-ghobreil1@ikea.com>
2021-10-21 21:37:10 +0200

    commit for external function call

---
+++ src/main/java/com/drools/springdrools/service/HelloCity.java
@@ -0,0 +1,8 @@
+package com.drools.springdrools.service;
+
+public class HelloCity {
+
+    public static void writeHello(String name) {
+        System.out.println("HELLO " + name + "!!!!!!");
+    }
+}
